0bb81dc677ae274ac4d26295368c8e11f911a91b
Paschoud Van Zyl <paschoudv@gmail.com>
2019-09-14 23:36:08 -0600

    Initial commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text finalScore;
+
+    void Start()
+    {
+        finalScore.text = Score.PinCount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/buttonM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonM : MonoBehaviour
+{
+    public void quitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+    
+    public void startSlow()
+    {
+        Rotator.speed = 50f;
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void startFast()
+    {
+        Rotator.speed = 150f;
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void loadEnd()
+    {
+              SceneManager.LoadScene("end");
+    }
+}
